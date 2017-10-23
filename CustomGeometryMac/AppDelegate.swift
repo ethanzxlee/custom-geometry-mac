3a615074a8a4ecd83c854e36938896f7c61e33c4
Zhe Xian Lee <zxlee618@gmail.com>
2017-10-24 09:04:36 +1100

    Initial Commit

---
+++ CustomGeometryMac/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CustomGeometryMac
+//
+//  Created by Zhe Xian Lee on 24/10/17.
+//  Copyright © 2017 Zhe Xian Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
